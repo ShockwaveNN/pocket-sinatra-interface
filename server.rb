82df55cdcd28c0264fe685869a10ff4e4a088f92
Pavel Lobashov <shockwavenn@gmail.com>
2016-07-19 19:24:45 +0300

    Fix rubocop problems

--- server.rb
+++ server.rb
@@ -5,9 +5,7 @@ require 'json'
 require 'pocket-ruby'
 config_file 'config.yaml'
 
-
 enable :sessions
-
 
 def callback_url
   "#{request.base_url}/oauth/callback"
@@ -24,12 +22,12 @@ Pocket.configure do |config|
 end
 
 get '/reset' do
-  puts "GET /reset"
+  puts 'GET /reset'
   session.clear
 end
 
-get "/" do
-  puts "GET /"
+get '/' do
+  puts 'GET /'
   puts "session: #{session}"
 
   if session[:access_token]
@@ -43,35 +41,36 @@ get "/" do
   end
 end
 
-get "/oauth/connect" do
-  puts "OAUTH CONNECT"
-  session[:code] = Pocket.get_code(:redirect_uri => callback_url)
-  new_url = Pocket.authorize_url(:code => session[:code], :redirect_uri => callback_url)
+get '/oauth/connect' do
+  puts 'OAUTH CONNECT'
+  session[:code] = Pocket.get_code(redirect_uri: callback_url)
+  new_url = Pocket.authorize_url(code: session[:code],
+                                 redirect_uri: callback_url)
   puts "new_url: #{new_url}"
   puts "session: #{session}"
   redirect new_url
 end
 
-get "/oauth/callback" do
-  puts "OAUTH CALLBACK"
+get '/oauth/callback' do
+  puts 'OAUTH CALLBACK'
   puts "request.url: #{request.url}"
   puts "request.body: #{request.body.read}"
-  result = Pocket.get_result(session[:code], :redirect_uri => callback_url)
+  result = Pocket.get_result(session[:code], redirect_uri: callback_url)
   session[:access_token] = result['access_token']
   puts result['access_token']
   puts result['username']
   puts session[:access_token]
   puts "session: #{session}"
-  redirect "/"
+  redirect '/'
 end
 
 get '/add' do
-  client = Pocket.client(:access_token => session[:access_token])
-  info = client.add :url => 'http://getpocket.com'
+  client = Pocket.client(access_token: session[:access_token])
+  info = client.add url: 'http://getpocket.com'
   "<pre>#{info}</pre>"
 end
 
-get "/upload" do
+get '/upload' do
   if logged_in?
     haml :no_key
   else
@@ -84,14 +83,14 @@ get '/upload/incorrect-json' do
 end
 
 # Handle POST-request (Receive and save the uploaded file)
-post "/upload" do
-  client = Pocket.client(:access_token => session[:access_token])
+post '/upload' do
+  client = Pocket.client(access_token: session[:access_token])
   filename = 'uploads/' + params['myfile'][:filename]
-  File.open(filename, "w") do |f|
+  File.open(filename, 'w') do |f|
     f.write(params['myfile'][:tempfile].read)
   end
   begin
-    json_content = JSON.parse( File.read(filename, :encoding => 'bom|utf-8'))
+    json_content = JSON.parse(File.read(filename, encoding: 'bom|utf-8'))
   rescue JSON::ParserError
     redirect 'upload/incorrect-json'
   end
@@ -101,9 +100,8 @@ post "/upload" do
   end
   added_url_data = ''
   urls.each do |cur_url|
-    info = client.add :url => cur_url
+    info = client.add url: cur_url
     added_url_data += "<p>Added: #{info}</p>"
   end
   added_url_data
 end
-
