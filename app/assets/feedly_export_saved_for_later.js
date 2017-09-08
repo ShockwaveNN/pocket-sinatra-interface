cfa5dd700e9233788148ba99d6e82b27f4776b78
Pavel Lobashov <shockwavenn@gmail.com>
2017-09-08 12:34:08 +0300

    Update js file for new feedly design

--- app/assets/feedly_export_saved_for_later.js
+++ app/assets/feedly_export_saved_for_later.js
@@ -9,7 +9,6 @@
 //    {
 //      title: "Title",
 //      url: "www.example.com/title",
-//      time: "Sunday "
 //    }
 //  ]
 //
@@ -22,41 +21,38 @@
 // 6) Paste the script below into the console
 //
 // NOTE: You must switch off SSL (http rather than https) or jQuery won't load!
- 
+
 // Feedly doesn't use jQuery so firstly inject it into the page
- 
-function loadJQuery(){
-  script = document.createElement('script');
-  script.setAttribute('src', '//code.jquery.com/jquery-2.1.3.js');
-  script.setAttribute('type', 'text/javascript');
-  script.onload = loadSaveAs;
-  document.getElementsByTagName('head')[0].appendChild(script);
+
+function loadJQuery() {
+    script = document.createElement('script');
+    script.setAttribute('src', '//code.jquery.com/jquery-2.1.3.js');
+    script.setAttribute('type', 'text/javascript');
+    script.onload = loadSaveAs;
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
- 
-function loadSaveAs(){
-  saveAsScript = document.createElement('script');
-  saveAsScript.setAttribute('src', 'https://rawgit.com/eligrey/FileSaver.js/master/FileSaver.js');
-  saveAsScript.setAttribute('type', 'text/javascript');
-  saveAsScript.onload = saveToFile;
-  document.getElementsByTagName('head')[0].appendChild(saveAsScript);
+
+function loadSaveAs() {
+    saveAsScript = document.createElement('script');
+    saveAsScript.setAttribute('src', 'https://rawgit.com/eligrey/FileSaver.js/master/FileSaver.js');
+    saveAsScript.setAttribute('type', 'text/javascript');
+    saveAsScript.onload = saveToFile;
+    document.getElementsByTagName('head')[0].appendChild(saveAsScript);
 }
- 
+
 function saveToFile() {
-  // Loop through the DOM, grabbing the information from each bookmark
-  map = jQuery("#section0_column0 div.u0Entry").map(function(i, el) {
-    var $el = jQuery(el);
-    var regex = /published:(.*)\ --/i;
-    return {
-      title: $el.data("title"),
-      url: $el.data("alternate-link"),
-      time: regex.exec($el.find("div.lastModified span").attr("title"))[1]
-    };
-  }).get(); //  Convert jQuery object into an array
- 
-  // Convert to a nicely indented JSON string
-  json = JSON.stringify(map, undefined, 2);
-var blob = new Blob([json], {type: "text/plain;charset=utf-8"});
-saveAs(blob, "FeedlySavedForLater" + Date.now().toString() + ".txt");
+    // Loop through the DOM, grabbing the information from each bookmark
+    map = jQuery(".list-entries .entry").map(function (i, el) {
+        return {
+            title: el.getAttribute('data-title'),
+            url: el.getAttribute("data-alternate-link")
+        };
+    }).get(); // Convert jQuery object into an array
+
+    // Convert to a nicely indented JSON string
+    json = JSON.stringify(map, undefined, 2);
+    var blob = new Blob([json], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "FeedlySavedForLater" + Date.now().toString() + ".txt");
 }
- 
-loadJQuery()
+
+loadJQuery();
