25d331f4f8d686d332f25bb5743c5c48d5135b7a
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-09-02 12:48:19 +0300

    Add codecov support (#68)
    * Add codecov support
    
    * Add codecov github action
    
    * Require simplecov
    
    * Temproaly disable ci flags
    
    * Add file command
    
    * Fix more
    
    * Add debug command to ouptut coverage result
    
    * Update codecov to latest version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# if ENV['CI'] == 'true'
+require 'simplecov'
+SimpleCov.start
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+# end
+
 require 'rack/test'
 require 'rspec'
 
