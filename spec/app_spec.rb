65b9921f68c782cde8ecf4ad3f0e86e5a2edc397
Pavel Lobashov <shockwavenn@gmail.com>
2021-07-01 13:29:18 +0300

    Fix rubocop `RSpec/ExampleWording` cop

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe 'My Sinatra Application' do
-  it 'should allow accessing the home page' do
+  it 'allows accessing the home page' do
     get '/'
     expect(last_response).to be_ok
   end
